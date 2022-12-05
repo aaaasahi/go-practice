09e0977415b88a29357f19853e60165abcc20c29
aaaasahi <asapjorf1017@gmail.com>
2022-12-05 09:49:21 +0900

    チャネルとゴルーチン

---
+++ section9/9.main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// channel
+// チャネルとゴルーチン
+
+// チャネルを受信する関数
+func reciever(c chan int) {
+	for {
+		i := <-c
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// 並行
+	go reciever(ch1)
+	go reciever(ch2)
+
+	i := 0
+	for i < 100 {
+		ch1 <- i
+		ch2 <- i
+		time.Sleep(50 * time.Millisecond)
+		i++
+	}
+}
