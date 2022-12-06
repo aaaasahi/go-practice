7b29b35b636d8ec7ffef5b9c595130b3c7111c4a
aaaasahi <asapjorf1017@gmail.com>
2022-12-06 15:57:33 +0900

    ポインタ

---
+++ section10/1.main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func Double(i int) {
+  i = i * 2
+}
+
+// ポインタ型の引数を取る関数
+func Doublev2(i *int) {
+  *i = *i * 2
+}
+
+func Doublev3(s []int) {
+  for i, v := range s {
+    s[i] = v * 2
+  }
+}
+
+func main() {
+  var n int = 100
+  fmt.Println(n)
+
+  // アドレス
+  fmt.Println(&n) // 0x1400001a0a0
+
+  Double(n)
+
+  fmt.Println(n) // 100 関数で二倍してもnは100のまま
+
+  // ポインタを宣言
+  var p *int = &n
+  fmt.Println(p) // 0x1400001a0a0
+  fmt.Println(*p) // 100
+
+  // nの値を置き換えられる
+  *p = 300
+  fmt.Println(n) // 300
+  n = 200
+  fmt.Println(*p) // 200
+
+  // 値が置き換わる
+  Doublev2(&n)
+  fmt.Println(n) // 400
+
+  Doublev2(p)
+  fmt.Println(*p) // 800
+
+  // スライスなどの参照型はデフォルトでポインタの機能がある
+  var sl []int = []int{1, 2, 3}
+  Doublev3(sl)
+  fmt.Println(sl) // [2 4 6] ポインタにしなくても値が更新される
+}
